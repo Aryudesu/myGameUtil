043d9065afac0e31f344c91574013ae5294a433a
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-06 02:05:25 +0900

    feat: expose remaining utilities from umbrella header

--- include/mygame/myGameUtil.h
+++ include/mygame/myGameUtil.h
@@ -2,6 +2,11 @@
 
 #include "audio/SoundManager.h"
 #include "config/IniConfig.h"
+#include "core/Singleton.h"
 #include "debug/Logger.h"
+#include "encoding/Base64.h"
 #include "graphics/ImageManager.h"
 #include "input/InputManager.h"
+#include "save/SaveData.h"
+#include "scene/Scene.h"
+#include "scene/SceneManager.h"
